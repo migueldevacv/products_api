8a53e0d4265e9bb8c6cb2481c358f3ee7be6699c
Miguel Angel Castro <mikeangel7879@gmail.com>
2025-01-23 23:09:38 -0600

    [UPD] AÑADIR EL STATUS LABEL A LA CATEGORIA

--- app/Models/Catalogs/Category.php
+++ app/Models/Catalogs/Category.php
@@ -12,4 +12,13 @@ class Category extends Model
     ];
 
     public const WITHOUT_CATEGORY = 1;
+    
+    protected $appends = [
+        'status_label',
+    ];
+
+    public function getStatusLabelAttribute(): string
+    {
+        return $this->attributes['status'] === 1 ? 'ACTIVE' : 'INACTIVE';
+    }
 }
